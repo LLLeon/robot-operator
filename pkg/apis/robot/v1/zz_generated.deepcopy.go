db17b1aa861f8bd100bb513d1d3b42d33cd68a9d
LLLeon <jiachenhui1989@gmail.com>
2021-04-09 17:25:55 +0800

    Regenerate code

--- pkg/apis/robot/v1/zz_generated.deepcopy.go
+++ pkg/apis/robot/v1/zz_generated.deepcopy.go
@@ -56,7 +56,7 @@ func (in *Robot) DeepCopyObject() runtime.Object {
 func (in *RobotList) DeepCopyInto(out *RobotList) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
-	out.ListMeta = in.ListMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
 		*out = make([]Robot, len(*in))
